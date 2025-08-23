36af7387301f72df748d6e3f4bacb39a15d0e603
kamal gupta <kg381852@gmail.com>
2025-08-23 11:10:22 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,13 +47,43 @@ class ModelTrainer:
                 "CatBoost regressor": CatBoostRegressor()
 
             }
-
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "K-Nearest Regressor": {  # ✅ added
+                    'n_neighbors': [3, 5, 7, 9]
+                },
+                "XGBoost": {  # ✅ match name
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoost regressor": {  # ✅ match name
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "Adaboost regressor": {  # ✅ match name
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
             model_report: dict = evaluate_models(
                 x_train=x_train,
                 y_train=y_train,
                 x_test=x_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                params=params
             )
             ## to get the best model score from dict
             best_model_score=max(sorted(model_report.values()))
